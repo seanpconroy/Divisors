a38d435a80ceb1373901a1da092f9e459247baf7
seanpconroy <59537930+seanpconroy@users.noreply.github.com>
2022-05-18 14:05:08 -0400

    Add files via upload

--- divisors.py
+++ divisors.py
@@ -8,4 +8,19 @@ def find_divisors(value):
 			print (i,end=" ")
 		i = i + 1
 
+
+def countMultiples(value):
+    res = 0;
+    for i in range(1, value + 1):
+        if (i % 3 == 0 or i % 7 == 0):
+            print (i,end=" ")
+            res += 1;
+  
+    
+
+print("Divisors: ")
 find_divisors(value)
+
+print("\nCount of multiples of 3 or 7 or both: ")
+countMultiples(value)
+
